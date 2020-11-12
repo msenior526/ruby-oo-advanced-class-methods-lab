920126395e15a7b93d0fa2c02763db7be0f38050
msenior526 <msenior526@gmail.com>
2020-11-11 20:11:25 -0500

    commit tree b7d67299484187efded61f7eab8c82e525d9dc6f

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +11,55 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    self.all << song
+      song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|n| n.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) ? self.find_by_name(name) : self.create_by_name(name)
+    end
+
+  def self.alphabetical
+    self.all.sort_by {|n| n.name}
+  end
+
+  def self.new_from_filename(name)
+        song = Song.new
+        array = name.split(" - ")
+        song.name = array[1].chomp(".mp3")
+        song.artist_name = array[0]
+        song
+  end
+
+  def self.create_from_filename(name)
+        song = Song.new
+        array = name.split(" - ")
+        song.name = array[1].chomp(".mp3")
+        song.artist_name = array[0]
+        song.save
+        song
+  end
+
+  def self.destroy_all
+        self.all.clear
+end
 end
